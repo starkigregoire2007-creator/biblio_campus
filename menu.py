b976381d5884f60232f72bcc2712b8af9053569c
assogbamoncera <assogbamoncera@gmail.com>
2026-05-06 11:49:10 +0000

    Update menu.py
    nouvelle modification

--- menu.py
+++ menu.py
@@ -1,7 +1,4 @@
 def afficher_menu():
     print('=== BiblioCampus ===')
-    print('1. Gérer les livres')
-    print('2. Gérer les emprunts')
-    print('3. Voir les statistiques')
-    print('4. Quitter')
+    print('1. Gér
 
